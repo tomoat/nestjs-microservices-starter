fdf1d4b48dacbb0a4a55874412636232401d3d4c
tomato <ljc6qx@163.com>
2022-01-12 11:09:45 +0800

    add email example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata, ServerDuplexStream } from '@grpc/grpc-js';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +10,22 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @GrpcMethod('EmailService', 'sendEmail')
+  sendEmail(
+    data: { email: string },
+    metadata: Metadata,
+    call: ServerDuplexStream<any, any>,
+  ) {
+    const serverMetadata = new Metadata();
+
+    serverMetadata.add('Set-Cookie', 'yummy_cookie=choco');
+    call.sendMetadata(serverMetadata);
+
+    return {
+      status: 'success',
+      email: data.email,
+    };
   }
 }
